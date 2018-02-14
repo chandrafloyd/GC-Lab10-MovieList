a1ac7710b8078d3b1a87509eaa47a997629a2396
chandrafloyd <chandra_floyd@yahoo.com>
2018-02-14 07:21:24 -0500

    fixed it

---
+++ Lab 10-MovieList/Movies.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace MovieList
+{
+    class Movie
+    {
+
+        //FIELDS
+        private string name, category;
+
+        //PROPERTIES
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public string Category
+        {
+            set { category = value; }
+            get { return category; }
+        }
+
+
+        //CONSTRUCTOR
+
+        public Movie(string name, string category)
+        {
+            Name = name;
+            Category = category;
+        }
+
+        //CREATE THE MOVIE OBJECTS
+
+        public static List<Movie> MovieBuilder()  //had to create a method in order to bring the list to life
+        {
+          
+            List<Movie> movies = new List<Movie>
+                          
+            {
+                new Movie("Hoodwinked", "Animated"),   //this is the correct syntax for adding a new movie with type of 
+                new Movie("Shrek", "Animated"),
+                new Movie("Lego Batman", "Animated"),
+                new Movie("The Notebook", "Drama"),
+                new Movie("Dunkirk", "Drama"),
+                new Movie("The Color Purple", "Drama"),
+                new Movie("Stephen King's IT", "Horror"),
+                new Movie("The Shining", "Horror"),
+                new Movie("Nightmare on Elm Street", "Horror"),
+                new Movie("Little Shop of Horrors", "SciFi"),
+                new Movie("Star Wars", "SciFi"),
+                new Movie("Divergent", "SciFi")
+            
+        };
+         
+            return movies;
+    }
+
+        //DISPLAY MOVIES BY CATEGORY
+        // ENUM - Movie categories
+        enum CategoryList
+        {
+            Animated = 1,
+            Drama,
+            Horror,
+            SciFi,
+        };
+    }
+}
+
+
--- Lab 10-MovieList/Program.cs
+++ Lab 10-MovieList/Program.cs
@@ -5,170 +5,60 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using static System.Console;
 
-namespace Lab_10_MovieList
+namespace MovieList
 {
 
-    //CREATE MOVIE CLASS
-    class Movie
-    {
-        private string movieName;
-        private string movieCategory;
-        private int categoryNumber;
-
-
-        //CREATE GET/SET PROPERTIES IN ORDER TO RETRIEVE AND SET THE FIELD NAMES FOR EACH OBJECT
-        //public methods need to be created in order to write and read those fields (Name and Category)
-        public string Name
-        {
-            get
-            {
-                return movieName;
-            }
-            set
-            {
-                movieName = value;
-            }
-        }
-        public string Category
-        {
-            get
-            {
-                return movieCategory;
-            }
-            set
-            {
-                movieCategory = value;
-            }
-        }
-
-        public int categoryNumber
-        {
-            get
-            {
-                return categoryNumber;
-            }
-            set
-            {
-                categoryNumber = value;
-            }
-        }
-
-        //CREATE METHOD FOR CREATING THE INDVIDUAL MOVIE OBJECTS
-        //constructors
-
-        //CREATE THE MOVIE OBJECTS
-
-             }
-
-        }
-
-     
-    }
-
-
-
-    //METHOD CLASS
-    class Methods
-    {
-        //METHOD - EXIT PROGRAM
-        public static void ExitProgram()
-        {
-            bool ExitProgram;
-            string Continue;
-            if (Continue == ("no"))  //"N" || "n" 
-            {
-                ExitProgram = true;
-                Console.WriteLine("Goodbye!");
-            }
-        }
-
-    }
-
-
-    //VALIDATION CLASS
-    class Validations
-    {
-        //TO DO - add validation for y/n in exit program
-
-
-        //TO DO - add regex validation for category selection
-
-    }
-
-
-    //PROGRAM CLASS
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-        
-        //Horror
-        //movieList.Add("Saw", "Horror");
-        // movieList.Add("Stephen King's IT", "Horror");
-        movieList.Add("Nightmare on Elm Street");
-        movieList.Add("Get Out");
-        //Animated
-        movieList.Add("Mulan ");
-        movieList.Add("Tarzan");
-        movieList.Add("Shrek");
-        movieList.Add("Hoodwinked");
-        //Drama
-        movieList.Add("The Color Purple");
-        movieList.Add("The Boy in the Striped Pajamas");
-        movieList.Add("The Notebook");
-        movieList.Add("Wild");
-        //SciFi
-        movieList.Add("Maze Runner");
-        movieList.Add("Star Trek");
-        movieList.Add("Divergent");
-        movieList.Add("Star Wars");
+            List<Movie> movieObjects = Movie.MovieBuilder();
+            //have to make a list of the movieObject list so that it pull the list from movie class into program
 
+            //WELCOME
+            Console.WriteLine("Welcome to Tiny Netflix!");
 
-        Console.WriteLine("Welcome to the World's Smallest Netflix! \n Please choose a genre: \n");
-
+            //RUN PROGRAM
             bool ExitProgram = false;
             while (ExitProgram == false)
             {
+                //REQUEST CATEGORY
+                Console.WriteLine("\nPlease choose a category: \nAnimated, Drama, Horror, or SciFi\n");
+                string userChoice =  Console.ReadLine();
+
+                Console.WriteLine("\nExcellent choice! Here are your movies: \n");
 
 
-            //DISPLAY MOVIES BY CATEGORY
-            Console.WriteLine("Excellent choice! Here are your movies: \n");
-            // ARRAY - LIST OF MOVIES
 
-            
-            //FOR EACH MOVIE IN THE ARRAY, PRINT THE MOVIE TITLE IF THE USER-SELECTED CATEGORY MATCHES THE CATEGORY OF THE MOVIE
-            foreach object in
-
-                if (true)
+                //DISPLAY MOVIES
+           
+                foreach (Movie movies in movieObjects)
                 {
-
+                    if (movies.Category == userChoice)  //making this match the userchoice eliminates unnecessary if statements
+                        Console.WriteLine(movies.Name, movies.Category);
                 }
 
 
-                //CONTINUE? Y/N
-                #region ContinueYN
-                Console.WriteLine("Would you like to choose another category? \n");
+                //EXIT OR CONTINUE?
+                Console.WriteLine("\nWould you like to make another selection? y/n");
                 string Continue = Console.ReadLine();
-
-                if (Continue == "N")
-                {
-                    ExitProgram = true;
-                   // ExitProgram();
-                }
-
-                else
+                if (Continue == "y")
                 {
                     ExitProgram = false;
+                    
                 }
-                #endregion ContinueYN
+                else
+                {
+                    ExitProgram = true;
+                    Console.WriteLine("\nThank you for choosing Tiny Netflix! Please come again.\n");
+                }
+
             }
-
-
-
-         
         }
 
-
-
+    }
 
 }
+
+
+
